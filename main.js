a59d1cdb448a1bd998a4cc073df2ffc715ad726f
Rajni141 <76608979+Rajni141@users.noreply.github.com>
2021-11-08 20:09:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-function preload() {
+noseX=0;
+noseY=0;
 
+function preload() {
+clown_nose = loadImage('https://i.postimg.cc/Gm8W7b22/lipstick.png');
 }
 
 function setup() {
-canvas = createCanvas(350, 1350);
+canvas = createCanvas(300, 300);
 canvas.center();
 video = createCapture(VIDEO);
 video.size(300, 300);
@@ -15,6 +18,9 @@ poseNet.on('pose', gotPoses);
 
 function draw() {
 image(video, 0, 0, 300, 300);
+fill(255,0,0);
+stroke(255,0,0);
+image(clown_nose, noseX, noseY, 34, 30);
 }
 
 function take_snapshot(){
@@ -35,11 +41,14 @@ if(results.length > 0)
 {
 
 console.log(results);
-console.log("nose x = " + results[0].pose.nose.x);
-console.log("nose y = " + results[0].pose.nose.y);
+noseX = results[0].pose.nose.x -15;
+noseY = results[0].pose.nose.y +13;
+console.log("nose x = " + noseX);
+console.log("nose y = " + noseY);
 
 }
 
 
 
 }
+
